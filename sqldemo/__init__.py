8c1a61c2eb8d691bbe2d6f35fc6c70bc8f6aca96
Niall O'Higgins <niallo@unworkable.org>
2012-08-10 16:57:57 -0600

    create DB tables if they don't already exist.

--- sqldemo/__init__.py
+++ sqldemo/__init__.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import engine_from_config
 
-from .models import DBSession
+from .models import DBSession, Base
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -15,6 +15,8 @@ def main(global_config, **settings):
     env = get_paas_env(config)
     engine = create_engine(env.get_postgresql_url())
     DBSession.configure(bind=engine)
+    # Create tables if they don't already exist
+    Base.metadata.create_all(engine)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
